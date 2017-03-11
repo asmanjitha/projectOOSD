626c16806b78ecb3dd3ab91f5dc8d6a2f0de3548
DMadhuranga <kdanushka8@gmail.com>
2017-03-11 16:30:38 +0530

    Add files via upload

---
+++ Inventory/home.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+if (!isset($_SESSION['logged'])){
+	header('location:login.php');
+}
+echo "<b><big>Welcome </big></b></br> </br>";
+if($_SESSION['r_privillage']==0){
+	echo "<a href='newuser.php'>Create a new user</a></br>";
+}
+?>
+
+<html>
+<body>
+<a href = "logout.php">Log Out</a>
+</body>
+</html>
